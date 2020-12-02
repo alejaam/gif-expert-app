db2ded8d7850a81cd895a5e52e1a2ff1bcad279c
Alejandro <ale.jaam7@gmail.com>
2020-12-02 10:42:26 -0600

    custom Hooks

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
-import { GirGridItem } from './GirGridItem';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+// import { getGifs } from '../helpers/getGifs';
+// import { GirGridItem } from './GirGridItem';
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
+    const {data, loading} = useFetchGifs();
 
-    useEffect(() => {
-        getGifs(category).then(setImages);
-    }, [category]);
+    // useEffect(() => {
+    //     getGifs(category).then(setImages);
+    // }, [category]);
     
     return (
         <>
             <h3>{category}</h3>
-            <div className="card-grid">
+            {loading ? 'Cargando...' : 'Fin de carga'}
+            {/* <div className="card-grid">
                 {
                     images.map(img =>
                         <GirGridItem key={img.id} {...img} />
                     )
                 }
-            </div>
+            </div> */}
         </>
     )
 }
