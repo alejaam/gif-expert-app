902cd9c0eebceb0671c1074fb02808a8179e3d25
Alejandro <ale.jaam7@gmail.com>
2020-12-03 12:09:14 -0600

    Fixed https FetchAPI

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=Ccx08K44oZE5u1GL4d4EOuhejM2dStiO&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=Ccx08K44oZE5u1GL4d4EOuhejM2dStiO&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(img => {
